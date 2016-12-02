069af7fdcea7ab9bc6627427fc6acdd67abe9378
carlosgf <carlos.galvez-fernandez@williamhill.com>
2016-12-02 17:32:08 +0100

    client side signup forms

--- auth/src/components/auth/signin.js
+++ auth/src/components/auth/signin.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
 
 
 class Signin extends Component {
   handleFormSubmit({ email, password}) {
-    console.log(email, password);
+    this.props.signinUser({email, password});
   }
+
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return(
+        <div className="alert alert-danger">
+          <strong>Opps!</strong> { this.props.errorMessage}
+        </div>
+      );
+    }
+  }
+
   render() {
     const { handleSubmit, fields: { email, password }} = this.props;
     return (<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
@@ -15,15 +27,20 @@ class Signin extends Component {
       </fieldset>
       <fieldset className="form-group">
         <label>Password:</label>
-        <input {...password} className="form-control" />
+        <input {...password} type="password" className="form-control" />
       </fieldset>
+      {this.renderAlert()}
       <button action="submit" className="btn btn-primary">Sign In</button>
     </form>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
 export default reduxForm({
   form: 'signin',
   fields: ['email', 'password']
-})(Signin);
+}, mapStateToProps, actions)(Signin);
