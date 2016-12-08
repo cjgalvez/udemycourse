94c19b4aa8e2078f0f48f522f04aec106ee10115
carlosgf <carlos.galvez-fernandez@williamhill.com>
2016-12-08 15:47:13 +0100

    Authentication finished

--- auth/src/actions/index.js
+++ auth/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_MESSAGE } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -29,3 +29,38 @@ export function signoutUser() {
   localStorage.removeItem('token');
   return { type: UNAUTH_USER };
 }
+
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios.post(ROOT_URL + '/signup', { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch(response => dispatch(authError(response.response.data.error)));
+  }
+}
+
+export function fetchMessage() {
+  return function(dispatch) {
+    axios.get(ROOT_URL, {
+      headers: { authorization: localStorage.getItem('token') }
+    })
+      .then(response => {
+        dispatch({ type: FETCH_MESSAGE, payload: response.data.message })
+      });
+  }
+}
+
+// Version with redux promise
+//export function fetchMessage() {
+//  const request = axios.get(ROOT_URL, {
+//    headers: { authorization: localStorage.getItem('token')}
+//  })
+//
+//  return {
+//    type: FETCH_MESSAGE,
+//    payload: request
+//  };
+//}
